297befdfdffdb87877deb81c8b73e3478560498a
laxmantandon <laxmantandon@gmail.com>
2023-02-21 22:39:55 +0530

    Update whatsapp.py

--- geolife_customapp/geolife_customapp/whatsapp.py
+++ geolife_customapp/geolife_customapp/whatsapp.py
@@ -19,7 +19,7 @@ def send_account_block_whatsapp():
         return
     
     try:
-        customers = frappe.db.sql("SELECT name, mobile_no, customer_primary_contact from tabCustomer where mobile_no is not null", as_dict=1)
+        customers = frappe.db.sql("SELECT name, mobile_no, customer_primary_contact from tabCustomer where mobile_no is not null and mobile_no <> ''", as_dict=1)
         company = 'Geolife Agritech India Private Limited'
         
         for cust in customers:
@@ -76,11 +76,10 @@ def send_account_block_whatsapp():
                     { "body": html, "css": get_print_style(), "title": "Customer Due 150 Days"}
                 )
 
-                if cust.mobile_no != "" or cust.mobile_no:
-                    send_whatsapp_report(html, "Customer Due 150 Days", cust.customer_primary_contact, "150days", outstanding)
+                send_whatsapp_report(html, "Customer Due 150 Days", cust.customer_primary_contact, "150days", outstanding)
 
-                    doc = frappe.get_doc("Customer", cust.name)
-                    doc.add_comment("Comment", text=f"WHATSAPP for Account Block Notification Sent On {mobile_no}")
+                doc = frappe.get_doc("Customer", cust.name)
+                doc.add_comment("Comment", text=f"WHATSAPP for Account Block Notification Sent On {mobile_no}")
             
     except Exception as e:
         frappe.log_error(e, "whatsapp_for_account_block_log")
